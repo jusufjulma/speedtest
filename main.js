55b976e807f1a8900b41e3c5abe016a776a83467
jusufjulma <jusaleander@hotmail.com>
2018-09-18 13:03:14 +0300

    array methods fail

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 document.getElementById('startbutton').onclick = start;
 document.getElementsByClassName('stop')[0].onclick = stop;
@@ -7,30 +7,28 @@ const c2 = document.getElementById('c2');
 const c3 = document.getElementById('c3');
 const c4 = document.getElementById('c4');
 
-
 const blankCircles = document.getElementsByClassName('cBlank');
 let targetArray = [];
-let userArray = [];
+let score = 0;
 let loop;
-
-
 let delay = 1000;
+
 
 function start() {
   console.log("LOOPATAAN " + delay);
-  delay *= 0.98;
+  delay *= 0.99;
   loop = setTimeout(start, delay);
   targetFlash();
 }
 
 function stop() {
   console.log("Stopping...");
-  clearTimeout(loop)
+  clearTimeout(loop);
 }
 
 function targetFlash() {
   let flashMe = (Math.floor(Math.random()*4));      // this is just to clarify
-  targetArray.push(flashMe);                        // how it works; could be shorter
+  targetArray.unshift(flashMe);                        // how it works; could be shorter
   console.log(targetArray);
   blankCircles[flashMe].classList.add("cActive");
  if (delay < 300){
@@ -48,11 +46,31 @@ function logger(x) {
   console.log(x);
 }
 
-c1.addEventListener("mouseup", () => userPress(1))
-c2.addEventListener("mouseup", () => userPress(2))
-c3.addEventListener("mouseup", () => userPress(3))
-c4.addEventListener("mouseup", () => userPress(4))
+c1.addEventListener("mouseup", () => userPress(0))
+c2.addEventListener("mouseup", () => userPress(1))
+c3.addEventListener("mouseup", () => userPress(2))
+c4.addEventListener("mouseup", () => userPress(3))
 
 function userPress(x) {
-  console.log(x);
+  if (x !== targetArray[0]) {
+    stop();
+  }else {
+  targetArray.shift();
+  score++;
+  document.getElementById("scorevalue").innerHTML = score;
+  }
+}
+
+
+
+
+
+
+
+function musicplayer(x) {
+  if (x == 1){
+    endscreen.play()
+  }
+  endscreen.play()
+  console.log("ASDASD");
 }
