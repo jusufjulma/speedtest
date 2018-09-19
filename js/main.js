13f8a0564cd23c85a680f4786f8ae83a855653f8
jusufjulma <jusaleander@hotmail.com>
2018-09-19 08:06:53 +0300

    music and comments added

---
+++ js/main.js
@@ -0,0 +1,93 @@
+"use strict";
+
+// document.getElementsByClassName('stop')[0].onclick = stop;
+const c1 = document.getElementById('c1');   // making html elements more
+const c2 = document.getElementById('c2');   // accessible
+const c3 = document.getElementById('c3');
+const c4 = document.getElementById('c4');
+
+const blankCircles = document.getElementsByClassName('cBlank');
+let targetArray = [];                   // logs flashed buttons sequence
+let score = 0;
+let loop;                               // stores setTimeout function call
+let delay = 1000;                       // how long between flashes
+
+document.getElementById('retrybutton').onclick = () => window.location.reload(true);
+document.getElementById('startbutton').onclick = () =>{
+  start();
+  stopSound();
+  player(2);
+}
+
+function start() {
+  delay *= 0.98;                        // after 50 clicks this is about 350ms
+  loop = setTimeout(start, delay);      // recursion right there
+  targetFlash();                        // call the flasher
+}
+
+function stop() {                       // stops game
+  clearTimeout(loop);
+  stopSound();                          // stops music
+  endScreen();                          // pops endscreen
+  player(3);                            // ...and plays endscreen music
+}
+
+function targetFlash() {                // game engine
+  let flashMe = (Math.floor(Math.random()*4));      // picks one button
+  targetArray.push(flashMe);                        // adds it to array,
+  // console.log(targetArray);                      // LAST POSITION
+  blankCircles[flashMe].classList.add("cActive");   // adds actual flash with CSS
+ if (delay < 300){                                  // for HC gamers
+   setTimeout(removeFlash, delay-20, flashMe);
+ }else{
+    setTimeout(removeFlash, 300, flashMe);          // after 300ms removes flash
+ }
+}
+
+function removeFlash(x) {                           // removes active class
+  blankCircles[x].classList.remove("cActive");      // a.k.a flash
+}
+
+c1.addEventListener("mouseup", () => userPress(0))  // handlers for actual
+c2.addEventListener("mouseup", () => userPress(1))  // user input
+c3.addEventListener("mouseup", () => userPress(2))
+c4.addEventListener("mouseup", () => userPress(3))
+
+function userPress(x) {             // deals with user input
+  if (x !== targetArray[0]) {       // first we see if player fails
+    stop();                         // in which case game stops.
+  }else {
+  targetArray.shift();              // otherwise correct value, which is first
+  score++;                          // on array due to PUSH gets removed and
+  document.getElementById("scorevalue").innerHTML = score;  // score gets updated
+  }
+}
+
+function endScreen() {              // pops up end screen
+  let message;                      // initializing for different messages
+  document.getElementsByTagName('body')[0].style.overflow = "hidden";
+  if (score === 0){
+    message = `Were the instructions unclear?`
+  }else if (score < 6) {                  // which are progressively more cheerful
+    message = `Your final score was ${score}.
+    Are you proud of yourself now?`;
+    console.log(`Well, you scored ${score}. Congratulations.`);
+  }else if (score >= 6 && score < 20){
+    message = `You got ${score} clicks right... Well, it is not
+      exactly something to brag about.`
+  }else if (score >= 20 && score < 30){
+    message = `${score}, huh? Pretty good...`
+  }else if (score >= 30 && score < 50){
+    message = `${score}??? Wow. Maybe you should go and do something more
+    productive. Seriously.`         // or not
+  }else if (score >=50){
+    message = `GET OUT!<br>With that amount of dedication I expect you to be
+    able to open console which might answer your question.`;
+    console.log(`Well, you scored ${score}. Congratulations.`);
+  }
+  document.getElementsByClassName("aftermath")[0].style.visibility = "visible";
+  document.getElementById("finalscore").innerHTML = message; // CSS commands
+}
+
+// this game could have module to let user bind keys for buttons
+// also retry button functionality could be better
---
+++ js/music.js
@@ -0,0 +1,53 @@
+const intro = new Audio("music/intro.ogg");
+const mainloop = new Audio("music/mainloop.ogg");
+const gametime = new Audio("music/gametime.ogg");
+const maxspeed = new Audio("music/maxspeed.ogg");
+const endscreen = new Audio("music/endscreen.ogg");
+const songs = [intro, mainloop, gametime, maxspeed, endscreen];
+let mutestate = 0;    // keeps track of mute situation
+
+const mutesound = document.getElementById("mute").onclick = muteSound;
+
+function muteSound() {
+  if (mutestate === 0) {
+    for (let i = 0; i < songs.length; i++) {  // mutes every sound
+      songs[i].volume = 0;
+    }
+    mutestate++;                // updates muted situation
+  }else if (mutestate === 1) {
+    for (let i = 0; i < songs.length; i++) {
+      songs[i].volume = 1;
+    }
+    mutestate--;                // updates muted situation
+  }
+}
+
+let introcheck = 0;             // prevents intro from playing twice
+let stopcheck = 0;              // tells if stopSound has been triggered
+let mousemoves = 0;
+
+const enter = document.getElementsByClassName('hello')[0];
+enter.addEventListener("mouseup", () => player(0))  // handlers for actual
+
+function player(x) {                    // this function handles audio requests
+  if (x === 0 && introcheck === 0) {    // HINGEROOOOOOOOSTAS
+    intro.play();
+    introcheck++                        // intro has been played
+    setTimeout(() => player(1), 6850)   // and now moving to menu music
+    enter.style.visibility = "hidden";
+  }else if (x === 1 && stopcheck === 0) {   // which will not play if player
+    mainloop.loop = true;                   // started already. Otherwise loops
+    mainloop.play();
+  }else if (x === 2) {                      // gametime!
+    gametime.play()
+  }else if (x === 3) {                      // you win/lose
+    endscreen.play()
+  }
+}
+
+function stopSound() {                      // every switch needs this
+  stopcheck++;                              // informs that menumusic is obsolete
+  for (let i = 0; i < songs.length; i++) {  // pauses every audio
+    songs[i].pause();
+  }
+}
